a592e0df59b8a6e9d32996a2dcdbc31c6c18dd73
pigram86 <todd@toddpigram.com>
2013-11-29 10:10:09 -0500

    change

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,8 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-<<<<<<< HEAD
-=======
+
 # Citrix_Server_Tuning
 registry_key node[:tuning][:run] do
   action :create
